35160def6f559e77af446a4031e001cec0283b9d
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-09-25 23:20:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function createEmployeeRecord([firstName,lastName,title,payRate]){
+  let employee = {
+    firstName: firstName,
+    familyName: lastName,
+    title: title,
+    payPerHour: payRate,
+    timeInEvents:[],
+    timeOutEvents:[]
+  }
+  return employee
+};
+
+function createEmployeeRecords(arrayOfNames){
+  arrayOfNames.forEach(array => {
+    let fullRecords = createEmployeeRecord(array)
+    return fullRecords;
+  })
+};
