5d7f743020e0723f900196e2ec8c8e30b823cbfb
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-09-27 19:48:36 -0400

    commit tree 638c483c4d01bbf736ac5aa1ba3f1ca6165b5dfc

--- index.js
+++ index.js
@@ -12,6 +12,54 @@ function createEmployeeRecord([firstName,lastName,title,payRate]){
 
 function createEmployeeRecords(arrayOfNames){
   return arrayOfNames.map(array => {
-  createEmployeeRecord(array)
+  let records = createEmployeeRecord(array)
+  return records
   })
+};
+
+function createTimeInEvent(employeeRecord,timestamp){
+  employeeRecord.timeInEvents.push ({
+    type: "TimeIn",
+    date: timestamp.split(" ")[0],
+    hour: parseInt(timestamp.split(" ")[1], 10)
+    })
+    return employeeRecord
+};
+
+function createTimeOutEvent(employeeRecord,timestamp){
+  employeeRecord.timeOutEvents.push ({
+    type: "TimeOut",
+    date: timestamp.split(" ")[0],
+    hour: parseInt(timestamp.split(" ")[1], 10)
+  })
+  return employeeRecord
+};
+
+function hoursWorkedOnDate(employeeRecord,date){
+  let startingHourByDate = employeeRecord.timeInEvents.filter(user => user.date.startsWith(date));
+  let endingHourByDate = employeeRecord.timeOutEvents.filter(user => user.date.startsWith(date));
+  let totalHoursWorked = (endingHourByDate[0].hour - startingHourByDate[0].hour)/100
+
+  return totalHoursWorked;
+};
+
+function wagesEarnedOnDate(employeeRecord,date){
+  let hoursWorked = hoursWorkedOnDate(employeeRecord,date);
+  return hoursWorked * employeeRecord.payPerHour
+};
+
+function allWagesFor(employeeRecord){
+  let dailyWages = employeeRecord.timeInEvents.map(day => {
+    return wagesEarnedOnDate(employeeRecord,day.date)
+    });
+  let reducer = (firstValue,nextValue) => firstValue + nextValue;
+  let totalWages = dailyWages.reduce(reducer,0);
+  return totalWages;
+};
+
+function calculatePayroll([...employeeRecordArray]){
+  let totalWages = employeeRecordArray.map(employee => allWagesFor(employee));
+  let reducer = (firstValue,nextValue) => firstValue + nextValue;
+  let companyPayroll = totalWages.reduce(reducer,0);
+  return companyPayroll;
 };
